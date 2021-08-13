e27d025e857f46d7f1beb40d528e34ff5a57a031
MatheusSantosz <matheus.santos.ciencia@gmail.com>
2021-08-12 22:06:15 -0300

    completando sta2021

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -88,6 +88,29 @@ public class Pet {
                         .body("status", is("sold"))
             ;
 
-
         }
+ @Test(priority = 4)
+ public void excluirPet(){
+     String petId = "2018040727";
+
+     given()
+             .contentType("application/json")
+             .log().all()
+     .when()
+             .delete(uri + "/" + petId)
+     .then()
+            .log().all()
+            .statusCode(200)
+            .body("code", is(200))
+            .body("type", is("unknown"))
+            .body("message", is(petId))
+
+
+             ;
+
+
+}
+
+
+
 }
